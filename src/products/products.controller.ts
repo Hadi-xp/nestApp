d46a24ed523a0d881e5b6c1bca7750122aa99b15
Hadixp <hadi.b.2002@gmail.com>
2026-06-28 22:54:32 +0430

    Update products.controller.ts

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -16,7 +16,7 @@ export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  create(@Body()createProductDto:CreateProductDto) {
+  create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.create(createProductDto);
   }
 
