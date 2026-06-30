f66caa05c1b271e41e703e47dc073689b1901956
Hadixp <hadi.b.2002@gmail.com>
2026-07-01 02:30:11 +0430

    db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { LoggerMiddleware } from './logger/logger.middleware';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [UsersModule, ProductsModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nestApp')
+  ],
   controllers: [AppController]
 })
 export class AppModule implements NestModule {
